c1901e6b9fdafda912f7156d586a69e03df947e1
egeucak <egeucak75@hotmail.com>
2019-11-21 16:08:48 +0300

    Fixed thread not stopping

--- MemLimiter.py
+++ MemLimiter.py
@@ -20,16 +20,16 @@ def __memory_monitor(command_queue: Queue, poll_interval=1, memory_limit=512):
             snapshot2 = tracemalloc.take_snapshot()
             top_stats = snapshot2.compare_to(snapshot1, 'lineno')
             total = sum(stat.size for stat in top_stats) / (1024 * 1024) # bytes to mb
+            print(total)
             if total > memory_limit:
                 command_queue.put(total)
                 return _thread.interrupt_main()
 
-def LimitMemory(mem_limit):
+def LimitMemory(mem_limit, poll_interval = 1):
     def limit_memory(func):
         @functools.wraps(func)
         def limiter(*args, **kwargs):
             queue = Queue()
-            poll_interval = 0.1
             exception = False
             monitor_thread = Thread(target=__memory_monitor, args=(queue, poll_interval, mem_limit))
             monitor_thread.start()
@@ -37,9 +37,10 @@ def LimitMemory(mem_limit):
                 val = func(*args, **kwargs)
                 return val
             except KeyboardInterrupt:
+                exception = True
+            finally:
                 queue.put('stop')
                 monitor_thread.join()
-                exception = True
             if exception: raise MemoryUsageExceeded('Your limit was {} mb, but the function consumed {} mb memory'.format(mem_limit, queue.get()))
         return limiter
     return limit_memory
